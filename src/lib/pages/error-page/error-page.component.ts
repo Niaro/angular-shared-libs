175d9be579073b4726b98813836e9d8f3ad66a82
George Kayzer <niaronir@gmail.com>
2018-12-12 02:58:03 +0500

    lint chore

--- src/lib/pages/error-page/error-page.component.ts
+++ src/lib/pages/error-page/error-page.component.ts
@@ -6,4 +6,4 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 	styleUrls: ['./error-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ErrorPageComponent{ }
+export class ErrorPageComponent { }
