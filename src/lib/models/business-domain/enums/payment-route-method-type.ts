470677c3e2fef8c9deb1971ada36153d780ce5b1
George Kayzer <niaronir@gmail.com>
2019-01-03 04:39:58 +0500

    icons

--- src/lib/models/business-domain/enums/payment-route-method-type.ts
+++ src/lib/models/business-domain/enums/payment-route-method-type.ts
@@ -4,7 +4,7 @@ export class PaymentRouteMethodType extends Enumeration {
 	static creditCard = new PaymentRouteMethodType('Credit Card');
 	static voucher = new PaymentRouteMethodType();
 	static wireTransfer = new PaymentRouteMethodType('Wire Transfer');
-	static bitcoin = new PaymentRouteMethodType();
-	static cap = new PaymentRouteMethodType('CAP');
+	static crypto = new PaymentRouteMethodType();
+	static ewallet = new PaymentRouteMethodType('EWallet');
 	static apm = new PaymentRouteMethodType('APM');
 }
