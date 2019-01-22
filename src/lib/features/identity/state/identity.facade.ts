741ee93f5243950d1676658110597d7c67ab09be
George Kayzer <niaronir@gmail.com>
2019-01-22 19:12:06 +0500

    Register User for Logrocket

--- src/lib/features/identity/state/identity.facade.ts
+++ src/lib/features/identity/state/identity.facade.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 
+import { TelemetryService } from '../../../providers';
 import { User } from '../models';
 import { IIdentityPartialState } from './identity.reducer';
 import { getUser } from './identity.selectors';
@@ -10,7 +11,9 @@ import * as Identity from './identity.actions';
 export class IdentityFacade {
 	user$ = this.store.pipe(select(getUser));
 
-	constructor(private store: Store<IIdentityPartialState>) { }
+	constructor(private store: Store<IIdentityPartialState>, private telemetry: TelemetryService) {
+		this.user$.subscribe(user => this.telemetry.registerUser(user.userName));
+	}
 
 	init(user: User) {
 		this.store.dispatch(new Identity.Init(user));
