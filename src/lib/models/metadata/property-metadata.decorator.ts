7d3a4f2ea3b5ea0d3b1b599506c81ebc37758594
George Kayzer <niaronir@gmail.com>
2019-03-26 20:50:03 +0500

    Menu on the merchant row

--- src/lib/models/metadata/property-metadata.decorator.ts
+++ src/lib/models/metadata/property-metadata.decorator.ts
@@ -1,7 +1,7 @@
 
 import { PropertyMetadata } from './property-metadata';
 import { MetadataEntity } from './metadata-entity';
-import { upperFirst } from 'lodash-es';
+import { startCase } from 'lodash-es';
 import { Omit } from '../misc/typescript-types';
 
 export function Property(config: Omit<PropertyMetadata, 'property'> = <any>{}) {
@@ -9,7 +9,7 @@ export function Property(config: Omit<PropertyMetadata, 'property'> = <any>{}) {
 		if (!(model instanceof MetadataEntity))
 			throw new Error('The property decorator can be set only for the class which extends the MetadataEntity class');
 
-		config.label = config.label || upperFirst(property);
+		config.label = config.label || startCase(property);
 		(<typeof MetadataEntity>model.constructor).metadata.push(new PropertyMetadata({ ...config, property }));
 	};
 }
