9c42668c817f582430a7ad58efe98567ad08ee01
George <niaronir@gmail.com>
2019-06-06 15:40:51 +0300

    feat: do not trust the back on number values

--- src/lib/models/metadata/mappers.ts
+++ src/lib/models/metadata/mappers.ts
@@ -1,7 +1,12 @@
 import { Country, Countries, CountryCode } from '../business-domain/countries';
+import { isNumber } from 'util';
 
 export function booleanMapper(v: any)  {
 	return v === 'true' || v === true;
+}
+
+export function numberMapper(v: any)  {
+	return isNumber(v) && !isNaN(v) ? v : 0;
 }
 
 export function countryMapper(v: Country | CountryCode) {
