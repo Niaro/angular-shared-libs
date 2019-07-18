c6060cd78f835b429edf0b0fb22e02f570c931ba
George <niaronir@gmail.com>
2019-07-18 14:20:06 +0300

    feat: audit logs

--- src/lib/utils/object-normalize.ts
+++ src/lib/utils/object-normalize.ts
@@ -1,16 +1,24 @@
-import { mapValues, isString } from 'lodash-es';
+import { mapValues, isString, isObject } from 'lodash-es';
+import { isArray } from 'util';
 
 /**
  * Turn all parseable string values into objects recursively;
  */
-export function normalize(object: any) {
-	return mapValues(object, (v) => {
-		try {
-			if (isString(v)) {
-				v = JSON.parse(v);
-				return normalize(v);
-			}
-		} catch (error) { }
-		return v;
-	});
+export function normalize(value: any) {
+	value = parse(value);
+	return isArray(value)
+		? value.map(v => normalize(v))
+		: isObject(value)
+			? mapValues(value, v => normalize(v))
+			: value;
+}
+
+function parse(value: any) {
+	try {
+		if (isString(value)) {
+			value = JSON.parse(value);
+			return value;
+		}
+	} catch (error) { }
+	return value;
 }
