5913099c6bf56868974c8fc1ed8abff1cc5b1cbb
Niaro <niaronir@gmail.com>
2019-11-05 23:25:30 +0500

    feat: a separate component for mounting the cashier on the admin

--- src/lib/models/business-domain/enums/cashier-theme.ts
+++ src/lib/models/business-domain/enums/cashier-theme.ts
@@ -3,5 +3,5 @@ import { Enumeration } from '../../misc/enums/enum';
 export class CashierTheme extends Enumeration {
 	static dark = new CashierTheme();
 	static light = new CashierTheme();
-	static virtualTerminal = new CashierTheme();
+	static admin = new CashierTheme();
 }
