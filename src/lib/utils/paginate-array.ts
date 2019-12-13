7740475545b895f978b0cfae9fb29a257e435c0c
Niaro <niaronir@gmail.com>
2019-12-13 21:55:52 +0500

    feat: handling sorting and paging on the perf report

---
+++ src/lib/utils/paginate-array.ts
@@ -0,0 +1,7 @@
+import { IPageQueryParams } from '../models/api';
+
+export function paginateArray(arr: any[], { page, limit }: IPageQueryParams) {
+	return page
+		? arr.slice((+page - 1) * +limit, +page * +limit)
+		: arr.slice(0, +limit);
+}
