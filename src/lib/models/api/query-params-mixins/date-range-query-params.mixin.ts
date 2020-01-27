8252028b5273058355a594489712920e9cbc841e
Niaro <niaronir@gmail.com>
2020-01-27 19:29:39 +0500

    fix(reports): #BDEV-814 perfomance report was ignoring date range

--- src/lib/models/api/query-params-mixins/date-range-query-params.mixin.ts
+++ src/lib/models/api/query-params-mixins/date-range-query-params.mixin.ts
@@ -13,8 +13,10 @@ export type DaterangeQueryParamsCtor = Constructor<IDaterangeQueryParams>;
 export function mixinDaterangeQueryParams<T extends Constructor<QueryParamsBase<{
 	dateRange?: string | DateRange,
 	range?: string | DateRange,
-	period?: string | DateRange
-	}>>>
+	period?: string | DateRange,
+	from?: string,
+	to?: string;
+}>>>
 	(base: T): DaterangeQueryParamsCtor & T {
 	return class extends base {
 		from!: number;
@@ -22,6 +24,12 @@ export function mixinDaterangeQueryParams<T extends Constructor<QueryParamsBase<
 
 		constructor(...args: any[]) {
 			super(...args);
+
+			if (this.routeParams.from)
+				this.from = +this.routeParams.from;
+
+			if (this.routeParams.to)
+				this.to = +this.routeParams.to;
 
 			const dateRange = this.routeParams.range || this.routeParams.period || this.routeParams.dateRange;
 			if (dateRange)
