820fd625248bb853bbd4a185307c7ea84f1a40e5
Niaro <niaronir@gmail.com>
2020-03-10 00:07:21 +0500

    feat(ma/payment-routes): a new iteration

--- src/lib/models/business-domain/enums/payment-method-type.ts
+++ src/lib/models/business-domain/enums/payment-method-type.ts
@@ -2,20 +2,24 @@ import { PspPaymentMethodType } from './psp-payment-method-type';
 
 export class PaymentMethodType extends PspPaymentMethodType {
 
-	static wireTransfer = new PaymentMethodType('Wire Transfer');
+	static wireTransfer = new PaymentMethodType(undefined, 'A set of bank account requisites per currency');
 
-	static externalLink = new PaymentMethodType();
+	static externalLink = new PaymentMethodType(undefined, 'A custom link to any internet resource');
 
-	static cryptoWallet = new PaymentMethodType('Crypto Wallets');
+	static cryptoWallet = new PaymentMethodType('Crypto Wallets', 'A set of crypto currency addresses');
+
+	static staticMethods = [
+		PaymentMethodType.externalLink,
+		PaymentMethodType.wireTransfer,
+		PaymentMethodType.cryptoWallet
+	];
 
 	static assignable = [
 		PaymentMethodType.creditCard,
 		PaymentMethodType.apm,
 		PaymentMethodType.crypto,
 		PaymentMethodType.voucher,
-		PaymentMethodType.externalLink,
-		PaymentMethodType.cryptoWallet,
-		PaymentMethodType.wireTransfer,
+		...PaymentMethodType.staticMethods
 	];
 
 	static staticPages = new PaymentMethodType('Pages');
@@ -24,5 +28,9 @@ export class PaymentMethodType extends PspPaymentMethodType {
 		PaymentMethodType.wireTransfer,
 		PaymentMethodType.cryptoWallet
 	];
+
+	static isStatic(type: PaymentMethodType) {
+		return PaymentMethodType.staticMethods.includes(type);
+	}
 
 }
