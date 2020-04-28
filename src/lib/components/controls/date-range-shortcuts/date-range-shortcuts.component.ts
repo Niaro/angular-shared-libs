33f85fe12a34eeeb2c8262a7ebae648ac0e06720
George <niaronir@gmail.com>
2020-04-28 18:19:55 +0500

    feat: date range present param

--- src/lib/components/controls/date-range-shortcuts/date-range-shortcuts.component.ts
+++ src/lib/components/controls/date-range-shortcuts/date-range-shortcuts.component.ts
@@ -2,9 +2,8 @@ import {
 	Component, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit, Input, HostBinding
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR } from '@angular/forms';
-import * as m from 'moment';
 
-import { DateRange, Enumeration, DateRangeInputValue } from '@bp/shared/models/misc';
+import { DateRange, DateRangeInputValue, DateRangeShortcut } from '@bp/shared/models/misc';
 import { lineMicrotask } from '@bp/shared/utils';
 
 import { ControlComponent } from '../control.component';
@@ -78,44 +77,5 @@ export class DateRangeShortcutsComponent extends ControlComponent<DateRange | nu
 
 		this.selected = value;
 		this.setValue(value && value.dateRange);
-	}
-}
-
-export class DateRangeShortcut extends Enumeration {
-
-	static week = new DateRangeShortcut();
-
-	static month = new DateRangeShortcut();
-
-	static quarter = new DateRangeShortcut();
-
-	static year = new DateRangeShortcut();
-
-	dateRange!: DateRange;
-
-	constructor() {
-		super();
-
-		lineMicrotask(() => {
-			this.dateRange = this._getDateRange()!;
-			setInterval(() => this.dateRange = this._getDateRange()!, 24 * 60 * 60 * 1000);
-		});
-	}
-
-	private _getDateRange() {
-		const to = m().endOf('day');
-
-		switch (this) {
-			case DateRangeShortcut.week:
-				return new DateRange({ to, from: m().utc().startOf('week') });
-			case DateRangeShortcut.month:
-				return new DateRange({ to, from: m().utc().startOf('month') });
-			case DateRangeShortcut.quarter:
-				return new DateRange({ to, from: m().utc().startOf('quarter') });
-			case DateRangeShortcut.year:
-				return new DateRange({ to, from: m().utc().startOf('year') });
-		}
-
-		throw new Error('DateRange for the DateRangeShortcut is not implemented');
 	}
 }
---
+++ src/lib/models/misc/date-range-shortcut.ts
@@ -0,0 +1,43 @@
+import * as m from 'moment';
+import { lineMicrotask } from '../../utils';
+import { Enumeration } from './enums';
+import { DateRange } from './date-range';
+
+export class DateRangeShortcut extends Enumeration {
+
+	static week = new DateRangeShortcut();
+
+	static month = new DateRangeShortcut();
+
+	static quarter = new DateRangeShortcut();
+
+	static year = new DateRangeShortcut();
+
+	dateRange!: DateRange;
+
+	constructor() {
+		super();
+
+		lineMicrotask(() => {
+			this.dateRange = this._getDateRange()!;
+			setInterval(() => this.dateRange = this._getDateRange()!, 24 * 60 * 60 * 1000);
+		});
+	}
+
+	private _getDateRange() {
+		const to = m().endOf('day');
+
+		switch (this) {
+			case DateRangeShortcut.week:
+				return new DateRange({ to, from: m().utc().startOf('week') });
+			case DateRangeShortcut.month:
+				return new DateRange({ to, from: m().utc().startOf('month') });
+			case DateRangeShortcut.quarter:
+				return new DateRange({ to, from: m().utc().startOf('quarter') });
+			case DateRangeShortcut.year:
+				return new DateRange({ to, from: m().utc().startOf('year') });
+		}
+
+		throw new Error('DateRange for the DateRangeShortcut is not implemented');
+	}
+}
