313518bf47366b2d3191726edbbaa4ae9d5c3519
George <niaronir@gmail.com>
2020-07-01 01:02:04 +0500

    fix(cloudflare): attempt to fix crazy redirects after cloudflare login

--- services/src/lib/cloudflare-access.service.ts
+++ services/src/lib/cloudflare-access.service.ts
@@ -1,10 +1,8 @@
-import { EMPTY, Observable, throwError, timer } from 'rxjs';
-import { concatMap, flatMap } from 'rxjs/operators';
+import { Observable, timer } from 'rxjs';
 
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { retryWithScalingDelay } from '@bp/shared/rxjs';
 import { uniqId } from '@bp/shared/utilities';
 
 import { BYPASS_AUTH_CHECK } from './http';
@@ -13,85 +11,40 @@ export const SKIP_CLOUDFLARE_ACCESS_CHECK = 'skip-cloudflare-access-check';
 
 export const CLOUDFLARE_ACCESS_CHECK_PATHNAME = 'cf-access-check';
 
-export const CLOUDFLARE_ACCESS_AUTHORIZED_PATHNAME = 'cdn-cgi/access/authorized';
-
 @Injectable({
 	providedIn: 'root'
 })
 export class CloudflareAccessService {
 
+	private _beforeRedirectHook?: () => void;
+
 	constructor(private _http: HttpClient) { }
 
-	whenUserUnathorizedByCloudflareRedirectToCloudflareLoginPage() {
+	whenUserUnathorizedByCloudflareRedirectToCloudflareLoginPage(
+		{ beforeRedirectHook }: { beforeRedirectHook?(): void; } = {}
+	) {
+		this._beforeRedirectHook = beforeRedirectHook;
+
 		timer(1000 * 60 * 2)
 			.subscribe(() => this.checkAccessAndTryRedirectToCFLogin());
 	}
 
-	async updateCloudflareAuthorizationCookie(): Promise<void> {
-		console.log('updateCloudflareAuthorizationCookie');
-		await timer(1000)
-			.pipe(
-				concatMap(() => this._tryGetCloudflareLoginUrl()
-					.pipe(flatMap(v => !!v.url
-						? throwError('Updating of cloudflare authorization cookie failed')
-						: EMPTY
-					))
-				),
-				retryWithScalingDelay({ scalingDelayDuration: 1000 })
-			)
-			.toPromise();
-	}
-
 	async checkAccessAndTryRedirectToCFLogin() {
-		console.log('checkAccessAndTryRedirectToCFLogin');
 		try {
 			const { url } = await this._tryGetCloudflareLoginUrl()
 				.toPromise();
 
-			if (url && !location.pathname.includes(CLOUDFLARE_ACCESS_AUTHORIZED_PATHNAME))
+			if (url) {
+				this._beforeRedirectHook && this._beforeRedirectHook();
 				location.href = url;
+			}
 		} catch (error) {
 
 		}
 	}
 
 	private _tryGetCloudflareLoginUrl(): Observable<{ url?: string; }> {
-		console.log('_tryGetCloudflareLoginUrl');
-
 		return this._http
 			.get(`/${ CLOUDFLARE_ACCESS_CHECK_PATHNAME }?cache-bust=${ uniqId() }&${ BYPASS_AUTH_CHECK }`);
 	}
-
-	/**
-	 * After we get redirected from the CF login page the app gets loaded by the worker
-	 * thus no request is being sent to the origin server which is proxyed by CF and updates CF_Authorization
-	 * cookie along the way. Thus to make a direct request to the origin server we are forced to unregister
-	 * the pwa worker and get the fresh data from the origin with the updated CF_Authorization cookie
-	 */
-	// async refreshCFAuthorizationCookie() {
-
-	// 	if (!navigator.serviceWorker || isDevMode())
-	// 		return;
-
-	// 	const ngswJS = `ngsw-worker.js`;
-
-	// 	const regs = await navigator.serviceWorker
-	// 		.getRegistrations();
-	// 	const ngsw = regs.find(v => v.active?.scriptURL.includes(ngswJS));
-
-	// 	if (ngsw) {
-	// 		if (this._hasCFAuthorizationCookie)
-	// 			this._isAuthorizedByCloudflare = true;
-	// 		else {
-	// 			ngsw.unregister();
-	// 			location.reload();
-	// 		}
-	// 	} else {
-	// 		// if after reload of the page we still don't have the cookie
-	// 		// means the domain under the CF Access bypass policy
-	// 		if (!this._hasCFAuthorizationCookie)
-	// 			this._isAuthorizedByCloudflare = true;
-	// 		navigator.serviceWorker.register(ngswJS);
-	// 	}
-	// }
 }
