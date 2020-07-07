ae095a7e52f2d5759135e17f31d537f3ba0984f0
George <niaronir@gmail.com>
2020-07-07 22:54:20 +0500

    fix(cloudflare-access): check access each hour

--- services/src/lib/cloudflare-access.service.ts
+++ services/src/lib/cloudflare-access.service.ts
@@ -17,7 +17,7 @@ export class CloudflareAccessService {
 	constructor(private _http: HttpClient) { }
 
 	whenUserUnathorizedByCloudflareRedirectToCloudflareLoginPage(): void {
-		timer(1000 * 60 * 60 * 1)
+		timer(0, 1000 * 60 * 60 * 1)
 			.subscribe(() => this.checkAccessAndTryRedirectToCFLogin());
 	}
 
