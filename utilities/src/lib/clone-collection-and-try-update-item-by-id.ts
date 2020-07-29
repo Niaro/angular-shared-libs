79450b91bcc8a6504afff303c0601c0002be5603
George <niaronir@gmail.com>
2020-07-29 21:19:14 +0500

    refactor(ma/payouts): update the present entity on the list on check transaction success

--- utilities/src/lib/clone-collection-and-try-update-item-by-id.ts
+++ utilities/src/lib/clone-collection-and-try-update-item-by-id.ts
@@ -1,11 +1,11 @@
 export function cloneCollectionAndTryUpdateItemById<T extends { id: any; }>(
 	collectionToClone: T[], itemToUpdate: T
 ): T[] {
-	const clone = collectionToClone.slice();
+	const shallowClone = collectionToClone.slice();
 
-	const entityArrayIndex = clone.findIndex(v => itemToUpdate.id === v.id);
+	const entityArrayIndex = shallowClone.findIndex(v => itemToUpdate.id === v.id);
 	if (entityArrayIndex !== -1)
-		clone.splice(entityArrayIndex, 1, itemToUpdate);
+		shallowClone.splice(entityArrayIndex, 1, itemToUpdate);
 
-	return clone;
+	return shallowClone;
 }
